135208eb5828c1fd7c08a2f13a192324d1ad247a
Rowlin10 <55354763+Rowlin10@users.noreply.github.com>
2019-09-15 17:28:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,5 @@ client.on('message', message => {
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.NjIyMDk0Njg3NjQ1NDY2NjM0.XXu5eA.MbwLpTWuhszCpDpLhS6aVYVlv0E);
+// THIS  MUST  BE  THIS  WAYclient.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
