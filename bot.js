4f64224c0bf9743a9abcf2d92a7ba194a3ee9ae5
Rowlin10 <55354763+Rowlin10@users.noreply.github.com>
2019-09-15 17:11:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
 });
 
-client.login('token');
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
